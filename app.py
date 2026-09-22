0b1a613e25b71fd61fcaddd0bd84ec34ee756e2b
abrahammoraxbox-ship-it <abrahammoraxbox@gmail.com>
2026-09-22 14:29:49 -0300

    Agregar archivos completos del proyecto Amamora

--- app.py
+++ app.py
@@ -8,20 +8,26 @@ app = Flask(__name__)
 def inicio():
     return render_template("index.html")
 
+
 # Categorías
 @app.route("/categorias")
 def categorias():
     return render_template("categorias.html")
+
 
 # Diseñador de Anillos
 @app.route("/anillos")
 def anillos():
     return render_template("anillos.html")
 
+
 # Diseñador de Collares
 @app.route("/collares")
 def collares():
     return render_template("collares.html")
+
+
+
 
 # Ejecutar la aplicación
 if __name__ == "__main__":
